644599dba1250085344b09001ea689dfd602d7c3
RedRelay <alexandre.duponchel@gmail.com>
2019-06-02 10:54:43 +0200

    Uses fixed Sponge GravityAffected property

--- src/main/java/fr/redrelay/spongecreeperheal/engine/dependency/DependencyEngineListeners.java
+++ src/main/java/fr/redrelay/spongecreeperheal/engine/dependency/DependencyEngineListeners.java
@@ -1,31 +1,31 @@
 package fr.redrelay.spongecreeperheal.engine.dependency;
 
 import fr.redrelay.spongecreeperheal.engine.dependency.factory.RelativePositionDependencyFactory;
-import net.minecraft.block.BlockFalling;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.data.property.block.GravityAffectedProperty;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
 import org.spongepowered.api.util.Direction;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class DependencyEngineListeners {
     private static final DependencyEngineListeners INSTANCE = new DependencyEngineListeners();
     private DependencyEngineListeners(){}
 
-    /**
-     * TODO : GravityAffected seems not to be implmented yet in Sponge, so we use an alternative using MNS. It should be replaced by Sponge API impl as soon as possible
-     * @param e
-     */
     @Listener
     public void registerFallingBlocks(GamePostInitializationEvent e) {
         final RelativePositionDependencyFactory fallingBlockDependencyFactory = new RelativePositionDependencyFactory(Direction.DOWN);
 
         final Collection<BlockType> blocks = Sponge.getRegistry().getAllOf(BlockType.class);
         blocks.parallelStream()
-                .filter(block -> BlockFalling.class.isAssignableFrom(block.getClass()))
+                .filter(block -> {
+                    final Optional<GravityAffectedProperty> opt = block.getProperty(GravityAffectedProperty.class);
+                    return opt.isPresent() && opt.get().getValue();
+                })
                 .collect(Collectors.toList())
                 .forEach(block -> {
                     DependencyEngine.getInstance().register(block, fallingBlockDependencyFactory);
