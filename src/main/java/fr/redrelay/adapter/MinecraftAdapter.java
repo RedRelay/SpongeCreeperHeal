c9f257e391565eaefa84f9dfde15b8517f50032b
RedRelay <alexandre.duponchel@gmail.com>
2019-06-02 12:00:39 +0200

    Adds Adapter

--- src/main/java/fr/redrelay/adapter/MinecraftAdapter.java
+++ src/main/java/fr/redrelay/adapter/MinecraftAdapter.java
@@ -1,4 +1,19 @@
 package fr.redrelay.adapter;
 
-public class MinecraftAdapter {
+import net.minecraft.block.BlockDoor;
+import org.spongepowered.api.block.BlockType;
+
+public final class MinecraftAdapter {
+
+    private static final MinecraftAdapter INSTANCE = new MinecraftAdapter();
+
+    private MinecraftAdapter() {}
+
+    public boolean isDoor(BlockType block) {
+        return BlockDoor.class.isAssignableFrom(block.getClass());
+    }
+
+    public static MinecraftAdapter getInstance() {
+        return INSTANCE;
+    }
 }
